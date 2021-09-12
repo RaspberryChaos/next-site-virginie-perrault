0e5f91bfe1f62dff751f94fec69a4498d14429f9
Ailie McCorkindale <ailiem134@hotmail.com>
2021-09-12 19:35:26 +0200

    qui suis je section added

---
+++ components/QuiSuisJe.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/QuiSuisJe.module.css';
+
+const QuiSuisJe = () => {
+    return (
+        <section id="qui-suis-je" className={`${styles.grid} purple-container`}>
+        <div className={styles.photoContainer}>
+          <Image
+            src={"/imgs/virginie.jpg"}
+            width={417}
+            height={627}
+            layout="responsive"
+            alt="Virginie Perrault"
+            className={styles.profilePic}
+          />
+        </div>
+
+        <div className={styles.about}>
+          <h1 className={styles.profileHeading}>Virginie Perrault</h1>
+          <p>
+            Je m’appelle Virginie, je suis âgée de 39 ans, je suis maman de deux
+            enfants, et cela fait maintenant un peu plus de 15 ans que je
+            travaille en tant qu’aide-soignante et auxiliaire de puériculture.
+            Avec mon savoir, mes connaissances et ma bienveillance, j’aimerais
+            accompagner les mamans à vivre leur maternité.
+          </p>
+
+          <h3 className={styles.profileHeading}>Mes Diplômes</h3>
+
+          <p>Diplôme d'État d'Auxiliaire de Puériculture</p>
+
+          <p>
+            Instructeur en Massage pour Bébé, International Association of
+            Infant Massage
+          </p>
+
+          <p>Massage Femme Enceinte selon Ayurvéda</p>
+        </div>
+      </section>
+    )
+}
+
+export default QuiSuisJe;
