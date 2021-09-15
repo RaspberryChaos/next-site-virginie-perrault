4a88d67d60e4d21ba185bf2a0e2cb6d3249a2d7b
Ailie McCorkindale <ailiem134@hotmail.com>
2021-09-15 21:50:20 +0200

    home icon added to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,67 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Nav.module.css";
 
-
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const [menuOpen, setMenuOpen] = useState(false);
-
-const handleClick = () => {
-    setMenuOpen(prev => !prev);
-}
+  const handleClick = () => {
+    setMenuOpen((prev) => !prev);
+  };
 
   return (
     <nav className={styles.mainNav}>
-        
-        <ul className={styles.navMenu} style={{right: (menuOpen ? "0" : "-100%")}}>
-          <li><a href="/#qui-suis-je" className={styles.navLink}>Qui suis-je?</a></li>
-          <li><a href="/#prestations" className={styles.navLink}>Prestations</a></li>
-          <li><a href="/#avis" className={styles.navLink}>Avis</a></li>
-          <li><a href="/#bons-cadeaux" className={styles.navLink}>Bons Cadeaux</a></li>
-          <li><a href="/#contact" className={styles.navLink}>Contact</a></li>
-        </ul>
-        <div className={menuOpen ? styles.hamburgerActive : styles.hamburger}  onClick={handleClick}>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
+      <ul
+        className={styles.navMenu}
+        style={{ right: menuOpen ? "0" : "-100%" }}
+      >
+        <Link href="/" passHref>
+          <a onClick={handleClick}>
+            <Image
+              src="/imgs/home-icon.png"
+              alt="Maison"
+              width={50}
+              height={50}
+            />
+          </a>
+        </Link>
+
+        <li>
+          <a href="/#qui-suis-je" className={styles.navLink} onClick={handleClick}>
+            Qui suis-je?
+          </a>
+        </li>
+        <li>
+          <a href="/#prestations" className={styles.navLink} onClick={handleClick}>
+            Prestations
+          </a>
+        </li>
+        <li>
+          <a href="/#avis" className={styles.navLink} onClick={handleClick}>
+            Avis
+          </a>
+        </li>
+        <li>
+          <a href="/#bons-cadeaux" className={styles.navLink} onClick={handleClick}>
+            Bons Cadeaux
+          </a>
+        </li>
+        <li>
+          <a href="/#contact" className={styles.navLink} onClick={handleClick}>
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div
+        className={menuOpen ? styles.hamburgerActive : styles.hamburger}
+        onClick={handleClick}
+      >
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
       </div>
-      </nav>
+    </nav>
   );
 };
 
